c34dc4b1c04c2f0eff67aedf39d8c1e834a11b5d
Vlad <Smirnov.ru92@mail.ru>
2023-12-24 22:56:16 +0500

    feat: read only example

--- dist/main.js
+++ dist/main.js
@@ -3,5 +3,10 @@ const s = {
     [n]: 'Vlad'
 };
 console.log(a);
-let a = { age: 1, voice: 1, lifes: 1 }; //in key of example
+let a = { age: 1, voice: 'voice', lifes: 3 }; //in key of example
+const b1 = { id: 100, title: 'p1', price: 1000 };
+let a1 = { id: 30, price: 1, title: "333" };
+const a2 = { title: '2' };
+let fedya = { lifes: 20, voice: 'meow' };
 export {};
+// fedya.lifes = 10 //readonly(невозможно изменить
--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,52 @@ const s = {
 
 console.log(a)
 
+// type Ronly<T> = {
+//     [K in keyof T]:T
+// }
+//
+// type rc = Ronly<ICat>
+//
+// let a:rc = {age:{age:1,voice:,lifes:1},voice:{age:1,voice:,lifes:1},lifes:{age:1,voice:,lifes:1}} //in key of example
+
+// type Ronly<T> = {
+//     [K in keyof T]:K
+// }
+//
+// type rc = Ronly<ICat>
+//
+// let a:rc = {age:'age',voice:'voice',lifes:'lifes'} //in key of example
+
+
 type Ronly<T> = {
-    [K in keyof T]:1
+    [K in keyof T]:T[K]
 }
 
 type rc = Ronly<ICat>
 
-let a:rc = {age:1,voice:1,lifes:1} //in key of example
+let a:rc = {age:1,voice:'voice',lifes:3} //in key of example
 
+const b1 = {id:100,title:'p1',price:1000}
+
+type rc1 = Ronly<typeof b1>
+
+let a1:rc1 = {id:30,price: 1,title: "333"}
+
+type Ronly1<T> = {
+    [K in keyof T]?:T[K]
+}
+
+const a2:Ronly1<typeof b1> = {title: '2'}
+
+type Cat = {
+    lifes:number,
+    voice:'meow'
+}
+
+type rdOnly<T> = {
+    readonly [K in keyof T]:T[K]
+}
+
+let fedya:rdOnly<Cat> = {lifes:20,voice:'meow'}
+
+// fedya.lifes = 10 //readonly(невозможно изменить
